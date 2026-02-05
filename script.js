6c77fed7f65a7256a4cacc7361b378247d07cf59
GuilhermeFRMDev <guilhermefrmdev@gmail.com>
2026-02-04 23:36:52 -0300

    Animaçoes e Loader

---
+++ script.js
@@ -0,0 +1,109 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
+
+ScrollSmoother.create({
+  smooth: 1.5,
+  effects: true,
+});
+
+function animarPagina() {
+  // Animações HERO
+
+  gsap.from(".hero", {
+    opacity: 0,
+    duration: 1,
+  });
+
+  gsap.from("picture:nth-child(2)", {
+    y: 60,
+    duration: 1,
+  });
+
+  gsap.from("picture:nth-child(1)", {
+    y: -60,
+    duration: 1,
+  });
+
+  // ANIMACOES CIDADES
+
+  gsap.from(".cidade", {
+    opacity: 0,
+    filter: "blur(10px)",
+    stagger: 0.3,
+    scrollTrigger: {
+      trigger: ".cidades",
+      start: "0% 80%",
+      end: "100% 70%",
+      scrub: true,
+    },
+  });
+
+  gsap.from(".secaoAgradecimentos ul li", {
+    opacity: 0,
+    x: 40,
+    filter: "blur(10px)",
+    stagger: 0.05,
+    scrollTrigger: {
+      trigger: ".secaoAgradecimentos ul",
+      start: "0% 80%",
+      end: "100% 50%",
+      scrub: true,
+    },
+  });
+
+  // ANIMACOES FOOTER
+
+  ScrollTrigger.refresh();
+
+  gsap.from("footer", {
+    y: "-30%",
+    immediateRender: false,
+    scrollTrigger: {
+      trigger: "footer",
+      scrub: true,
+      invalidateOnRefresh: true,
+      end: "100% 100%",
+    },
+  });
+
+  // LETRAS ANIMADAS
+  const grupoTextoSplit = document.querySelectorAll(".textoSplit");
+
+  grupoTextoSplit.forEach((textoUnicoSplit) => {
+    const split = SplitText.create(textoUnicoSplit, {
+      type: "lines, words, chars",
+      mask: "lines",
+    });
+
+    gsap.from(split.chars, {
+      y: 40,
+      opacity: 0,
+      duration: 0.3,
+      stagger: 0.03,
+      scrollTrigger: {
+        trigger: textoUnicoSplit,
+      },
+    });
+  });
+}
+
+//PRELOADER
+
+const tl = gsap.timeline({
+  onComplete() {
+    animarPagina()
+    gsap.to("#preloader", {
+      opacity: 0,
+      display: "none",
+    });
+  },
+});
+
+tl.to("#preloader path", {
+  duration: 1,
+  strokeDashoffset: 0,
+});
+tl.to("#preloader path", {
+  fill: "rgb(168, 19, 19)",
+  duration: 0.5,
+  strokeDashoffset: 0,
+});
